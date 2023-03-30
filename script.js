36c5304a5875b37673ebcc2008d9b4cdf34db8ff
timotoo <124346112+timotoo@users.noreply.github.com>
2023-03-30 14:08:00 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculate() {
+    let firstNumber =
+    parseInt(document.getElementById("firstNumber").value);
+    let secondNumber =
+    parseInt(document.getElementById("secondNumber").value);
+
+
+    document.getElementById("result").value = 
+    firstNumber + secondNumber;
+}
+
+let calculateButton = document.getElementById("calculateButton");
+calculateButton.value = "Calculate";
+calculateButton.style.fontSize = "20px";
+calculateButton.style.padding = "10px 50px";
+
+calculateButton.addEventListener("click", calculate);
